939e638738e81d2db618d5eee59fe2b532a14e23
Juplee <juplim@yahoo.co.id>
2026-05-22 09:42:07 +0700

    Update WalletConnect config with fallback and Hardhat
    Added Hardhat chain for local testing and provided a fallback projectId for WalletConnect.

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -3,7 +3,7 @@ import { sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "Prediction Market",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [sepolia],
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "c0f8641977717462c14175ed73010b98", //Enter a fallback projectId from WalletConnect to avoid application crash (error No projectId found ). 
+  chains: [hardhat,sepolia], //add Hardhat so that this application can i running and testing into my local host
   ssr: true,
 });
